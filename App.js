55ad1a1a5f13576bbe0bec7bfd44222b7363c0e5
Md Aamir Ashraf <132353345+baecareles@users.noreply.github.com>
2024-06-16 16:26:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import MortgageCalculator from './components/MortgageCalculator';
+import './App.css'
+
+function App() {
+return (
+	<div className="App">
+	<h1 className='gfg'>Dashboard</h1>
+	<MortgageCalculator />
+	</div>
+);
+}
+
+export default App;
